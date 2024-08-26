c62ee1d8c0890966c6fb4dfa49faf9967dd40c18
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-08-26 13:12:31 +0200

    remove unnecessary headers

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ with open("config.json","r") as f:
     config = json.load(f)
 
 skolportal_session = clam.SkolportalSession(config["username"], config["password"])
-#skola24_session = clam.Skola24Session(skolportal_session)
-#skola24_session.get_timetable()
 print(skolportal_session.get_user_attributes())
+#skola24_session = clam.Skola24Session(skolportal_session)
+#print(skola24_session.get_timetable(35,500,500,0)["data"].keys())
